cc1963cf5ecb7c9b0c4bbe0507d825d944c6cb79
TrinhThinh2004 <thinhtt2005@gmail.com>
2026-03-03 12:56:16 +0700

    update version next

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['bcryptjs', 'sequelize'],
-    eslint: {
-    ignoreDuringBuilds: true, 
-  },
   typescript: {
   ignoreBuildErrors: true,
 },
